ff90da9803fb9f4dd0e8db825a01e3c9c88e0a06
Tasnim Tanha <tasnimakthertanha@gmail.com>
2026-01-08 08:21:37 +0600

    solved problem 1

---
+++ problems.js
@@ -0,0 +1,14 @@
+
+ // Problem 1: Reverse a String
+
+ let str1 = "hello";
+
+ let reversed = "";
+
+  for (let i = str1.length - 1; i >= 0; i--) {
+  reversed = reversed + str1[i];
+ }
+
+console.log(reversed); 
+//Output will be : olleh
+
